86211110f07a1d63c8cba42bccbf103358855a22
M <krupnyak@hotmail.com>
2020-05-10 09:58:05 +0300

    conditions

---
+++ script.js
@@ -0,0 +1,12 @@
+let income = 50;
+
+(income > 20) ? console.log('hi') : console.log('hello');
+
+switch(income) {
+    case income < 49:
+    console.log('hi');
+    default:
+    console.log('hi');
+    break;
+
+}
